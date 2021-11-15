d719095e10e32d7fb9283fdf9d606dbb039ee8a1
furkan alahi <falahi1611212@bscse.uiu.ac.bd>
2021-11-15 22:01:17 +0000

    Spring Boot init

---
+++ src/main/java/com/bjss/desk_booking/DeskBookingApplication.java
@@ -0,0 +1,13 @@
+package com.bjss.desk_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeskBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeskBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bjss/desk_booking/DeskBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bjss.desk_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeskBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
