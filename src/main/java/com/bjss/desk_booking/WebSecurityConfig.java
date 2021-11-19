a2d5ca24e0c0e909a2664f7105548a9beae84890
Harvey Beynon <BeynonH3@cardiff.ac.uk>
2021-11-19 11:47:10 +0000

    Made login system for user and admin to display the templates

---
+++ src/main/java/com/bjss/desk_booking/WebSecurityConfig.java
@@ -0,0 +1,36 @@
+package com.bjss.desk_booking;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .mvcMatchers("/css/**").permitAll()
+                .mvcMatchers("/").permitAll()
+                .mvcMatchers("/public/**").permitAll()
+                .mvcMatchers("/admin/**").hasRole("ADMIN")
+                .mvcMatchers("/user/**").hasRole("USER")
+                .anyRequest().denyAll()
+                .and()
+                .formLogin();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("{noop}password").roles("USER");
+        auth
+                .inMemoryAuthentication()
+                .withUser("admin").password("{noop}admin").roles("ADMIN");
+    }
+}
