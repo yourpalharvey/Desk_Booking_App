cf8c7a57d4e11564ac0041b6c28448ceeec87311
c1522679 <shortmantc@cardiff.ac.uk>
2021-11-22 16:30:05 +0000

    removed unused imports

--- src/main/java/com/bjss/desk_booking/repository/DeskRepository.java
+++ src/main/java/com/bjss/desk_booking/repository/DeskRepository.java
@@ -1,8 +1,5 @@
 package com.bjss.desk_booking.repository;
 
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-
 //@Service
 public interface DeskRepository {
 
