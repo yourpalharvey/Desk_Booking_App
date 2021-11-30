66da304b5ecda0c1f8531ef7ba788bf5c08dbc91
Thomas Shortman <shortmantc@cardiff.ac.uk>
2021-11-30 13:47:37 +0000

    Created backend methods to get all bookings by date, create a new quick booking and get user bookings.

--- src/main/java/com/bjss/desk_booking/booking/BookingController.java
+++ src/main/java/com/bjss/desk_booking/booking/BookingController.java
@@ -1,4 +1,59 @@
 package com.bjss.desk_booking.booking;
 
+import com.bjss.desk_booking.desk.DeskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.sql.Date;
+import java.util.List;
+
+@Controller
 public class BookingController {
+
+    @Autowired
+    BookingService bookingService;
+
+    @Autowired
+    DeskService deskService;
+
+    // display bookings on the mybookings screen
+    // todo - update this method to only return bookings with user_id of current user
+    @GetMapping(value = "/user/mybookings")
+    public String myBookingStatus(Model model) {
+        List<Booking> bookingList = bookingService.findAll();
+
+        model.addAttribute("bookingList", bookingList);
+        return "MyBookingPage";
+    }
+
+    // display bookings based on the date chosen in calendar on user dashboard
+    @GetMapping(value = "user/getBookingsByDate")
+    public String bookingStatus(
+            Model model,
+            @RequestParam Date date
+    ) {
+        List<Booking> bookingList = bookingService.findAll();
+        List<Booking> bookingListByDate = new ArrayList<>();
+        System.out.println(date);
+
+        for (Booking b: bookingList) {
+            if (b.getDate() == date){
+                bookingListByDate.add(b);
+            }
+            System.out.println(b);
+        }
+
+        model.addAttribute("bookingListByDate", bookingListByDate);
+        return "BookingPage";
+
+    }
+
+    // Create a quick booking
+
+    //@PostMapping
 }
--- src/main/java/com/bjss/desk_booking/booking/BookingServiceImplementation.java
+++ src/main/java/com/bjss/desk_booking/booking/BookingServiceImplementation.java
@@ -1,4 +1,51 @@
 package com.bjss.desk_booking.booking;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class BookingServiceImplementation implements BookingService{
+
+    private BookingRepository bookingRepo;
+
+    @Autowired
+    public BookingServiceImplementation(BookingRepository bookingRepo){
+        this.bookingRepo = bookingRepo;
+    }
+
+    @Override
+    public List<Booking> findAll() {
+        return bookingRepo.findAll();
+    }
+
+    //if a booking with the selected id is found in the db, return Booking object.
+    //else, return null
+    @Override
+    public Booking findById(int bookingId) {
+        Optional<Booking> result = bookingRepo.findById(bookingId);
+
+        Booking booking = null;
+
+
+        if(result.isPresent()){
+            booking = result.get();
+        } else {
+            throw new RuntimeException("Did not find booking with ID: " + bookingId);
+        }
+
+        return booking;
+    }
+
+    @Override
+    public void save(Booking booking) {
+        bookingRepo.save(booking);
+    }
+
+    @Override
+    public void deleteById(int bookingId) {
+        bookingRepo.deleteById(bookingId);
+    }
 }
