fb470b5015f921a8b64d87507252c42dcec3bb64
Thomas Shortman <shortmantc@cardiff.ac.uk>
2021-11-30 13:47:37 +0000

    Created backend methods to get all bookings by date, create a new quick booking and get user bookings.

--- src/main/java/com/bjss/desk_booking/booking/BookingService.java
+++ src/main/java/com/bjss/desk_booking/booking/BookingService.java
@@ -1,4 +1,14 @@
 package com.bjss.desk_booking.booking;
 
+import java.util.List;
+
 public interface BookingService {
+
+    public List<Booking> findAll();
+
+    public Booking findById(int bookingId);
+
+    public void save(Booking booking);
+
+    public void deleteById(int bookingId);
 }
